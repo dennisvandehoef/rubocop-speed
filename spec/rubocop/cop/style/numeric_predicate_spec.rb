1213d0791aab0803b730ffeb52f41f3b4e84647b
Dennis van de Hoef <dennisvdhoef@gmail.com>
2016-10-15 14:09:59 +0200

    don't test unsuportet methods

--- spec/rubocop/cop/style/numeric_predicate_spec.rb
+++ spec/rubocop/cop/style/numeric_predicate_spec.rb
@@ -22,25 +22,27 @@ describe RuboCop::Cop::Style::NumericPredicate do
 
       expect(fastest(result)).to eql '!= 0'
     end
-    it '"> 0" is faster' do
-      result = Benchmark.ips do |x|
-        x.report('> 0') { RuboCop::Cop::Style::NumericPredicate.fast_positive}
-        x.report('positive?') { RuboCop::Cop::Style::NumericPredicate.slow_positive}
+    if target_ruby_version >= 2.3
+      it '"> 0" is faster' do
+        result = Benchmark.ips do |x|
+          x.report('> 0') { RuboCop::Cop::Style::NumericPredicate.fast_positive}
+          x.report('positive?') { RuboCop::Cop::Style::NumericPredicate.slow_positive}
 
-        x.compare!
+          x.compare!
+        end
+
+        expect(fastest(result)).to eql '> 0'
       end
+      it '"< 0" is faster' do
+        result = Benchmark.ips do |x|
+          x.report('< 0') { RuboCop::Cop::Style::NumericPredicate.fast_negative}
+          x.report('negative?') { RuboCop::Cop::Style::NumericPredicate.slow_negative}
 
-      expect(fastest(result)).to eql '> 0'
-    end
-    it '"< 0" is faster' do
-      result = Benchmark.ips do |x|
-        x.report('< 0') { RuboCop::Cop::Style::NumericPredicate.fast_negative}
-        x.report('negative?') { RuboCop::Cop::Style::NumericPredicate.slow_negative}
+          x.compare!
+        end
 
-        x.compare!
+        expect(fastest(result)).to eql '< 0'
       end
-
-      expect(fastest(result)).to eql '< 0'
     end
   end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,3 +5,9 @@ require 'benchmark/ips'
 def fastest(result)
   result.entries.sort_by{ |e| e.stats.central_tendency }.reverse.first.label
 end
+
+def target_ruby_version
+  @target_ruby_version ||= RuboCop::Config.new.target_ruby_version
+end
+
+puts "Target Ruby Version: #{target_ruby_version}"
