e7233313cc56aa536541e4a5326a058f645ac36e
Dennis van de Hoef <dennisvdhoef@gmail.com>
2016-10-15 13:56:40 +0200

    also test nonzero, positive and negative

--- lib/rubocop/cop/style/numeric_predicate.rb
+++ lib/rubocop/cop/style/numeric_predicate.rb
@@ -4,20 +4,82 @@ module RuboCop
   module Cop
     module Style
       class NumericPredicate
-        def self.slow
+        def self.slow_zero
           0.zero?
           1.zero?
           -1.zero?
           9558.zero?
+          -9558.zero?
           1565156.zero?
+          -1565156.zero?
         end
 
-        def self.fast
+        def self.fast_zero
           0 == 0
           1 == 0
           -1 == 0
           9558 == 0
+          -9558 == 0
           1565156 == 0
+          -1565156 == 0
+        end
+
+        def self.slow_nonzero
+          0.nonzero?
+          1.nonzero?
+          -1.nonzero?
+          9558.nonzero?
+          -9558.nonzero?
+          1565156.nonzero?
+          -1565156.nonzero?
+        end
+
+        def self.fast_nonzero
+          0 != 0
+          1 != 0
+          -1 != 0
+          9558 != 0
+          -9558 != 0
+          1565156 != 0
+          -1565156 != 0
+        end
+
+        def self.slow_positive
+          0.positive?
+          1.positive?
+          -1.positive?
+          9558.positive?
+          -9558.positive?
+          1565156.positive?
+          -1565156.positive?
+        end
+
+        def self.fast_positive
+          0 > 0
+          1 > 0
+          -1 > 0
+          9558 > 0
+          -9558 > 0
+          1565156 > 0
+          -1565156 > 0
+        end
+
+        def self.slow_negative
+          0.negative?
+          1.negative?
+          -1.negative?
+          9558.negative?
+          1565156.negative?
+        end
+
+        def self.fast_negative
+          0 < 0
+          1 < 0
+          -1 < 0
+          9558 < 0
+          -9558 < 0
+          1565156 < 0
+          -1565156 < 0
         end
       end
     end
--- spec/rubocop/cop/style/numeric_predicate_spec.rb
+++ spec/rubocop/cop/style/numeric_predicate_spec.rb
@@ -2,15 +2,45 @@ require 'spec_helper'
 
 describe RuboCop::Cop::Style::NumericPredicate do
   context 'Benchmark' do
-    it '""== 0" is faster' do
+    it '"== 0" is faster' do
       result = Benchmark.ips do |x|
-        x.report('== 0') { RuboCop::Cop::Style::NumericPredicate.fast}
-        x.report('zero?') { RuboCop::Cop::Style::NumericPredicate.slow}
+        x.report('== 0') { RuboCop::Cop::Style::NumericPredicate.fast_zero}
+        x.report('zero?') { RuboCop::Cop::Style::NumericPredicate.slow_zero}
 
         x.compare!
       end
 
       expect(fastest(result)).to eql '== 0'
+    end
+    it '!= 0" is faster' do
+      result = Benchmark.ips do |x|
+        x.report('!= 0') { RuboCop::Cop::Style::NumericPredicate.fast_nonzero}
+        x.report('nonzero?') { RuboCop::Cop::Style::NumericPredicate.slow_nonzero}
+
+        x.compare!
+      end
+
+      expect(fastest(result)).to eql '!= 0'
+    end
+    it '"> 0" is faster' do
+      result = Benchmark.ips do |x|
+        x.report('> 0') { RuboCop::Cop::Style::NumericPredicate.fast_positive}
+        x.report('positive?') { RuboCop::Cop::Style::NumericPredicate.slow_positive}
+
+        x.compare!
+      end
+
+      expect(fastest(result)).to eql '> 0'
+    end
+    it '"< 0" is faster' do
+      result = Benchmark.ips do |x|
+        x.report('< 0') { RuboCop::Cop::Style::NumericPredicate.fast_negative}
+        x.report('negative?') { RuboCop::Cop::Style::NumericPredicate.slow_negative}
+
+        x.compare!
+      end
+
+      expect(fastest(result)).to eql '< 0'
     end
   end
 end
